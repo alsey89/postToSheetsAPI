9ca9c2cc18b2b34bfef2571d241be49b2c7c806a
alsey89 <101123930+alsey89@users.noreply.github.com>
2023-05-10 23:33:59 +0800

    feat(post-api): add endpoint to append msgs to sheet, update API doc

--- README.md
+++ README.md
@@ -12,12 +12,12 @@ Create an API endpoint that posts messages into a Slack channel (or Google Sprea
 
 2. Set up an Express server
 
-   - [ ] Set up an API endpoint
-   - [ ] Code up a function that posts to Google Sheets
-   - [ ] Implement error handling
-   - [ ] Perform manual/Postman testing
-   - [ ] Write API documentation
-   - [ ] Commit
+   - [x] Set up an API endpoint
+   - [x] Code up a function that posts to Google Sheets
+   - [x] Implement error handling
+   - [x] Perform manual/Postman testing
+   - [x] Write API documentation
+   - [x] Commit
 
 3. Refactor and test the code
 
@@ -25,3 +25,94 @@ Create an API endpoint that posts messages into a Slack channel (or Google Sprea
    - [ ] Write automated tests using Jest
    - [ ] Update documentation to include testing instructions
    - [ ] Commit
+
+# Google Sheets API
+
+This API provides two endpoints to interact with Google Sheets, allowing users to read and write data.
+
+## Base URL
+
+pending
+
+## Endpoints
+
+### 1. Write data to Google Sheet
+
+Appends data to the specified range in the given Google Sheet.
+
+- **URL:** `/sheet/post`
+- **Method:** `POST`
+- **Content-Type:** `application/json`
+- **Body Parameters:**
+
+  | Parameter         | Type   | Description                                | Required |
+  | ----------------- | ------ | ------------------------------------------ | -------- |
+  | `spreadsheetLink` | string | The URL of the Google Sheet                | True     |
+  | `sender`          | string | Name of the sender                         | True     |
+  | `message`         | string | The message body                           | True     |
+  | `rangeToWrite`    | string | The range to write data (e.g. 'Sheet1!A1') | True     |
+
+- **Success Response:**
+
+  - **Code:** `200 OK`
+  - **Content Example:**
+
+    ```json
+    {
+      "message": {
+        "spreadsheetId": "1J1wqaKisURMwRKOjNS3e5Bme7KyMahrIB493Vd6_DJg",
+        "tableRange": "Messages!A1:B15",
+        "updates": {
+          "spreadsheetId": "1J1wqaKisURMwRKOjNS3e5Bme7KyMahrIB493Vd6_DJg",
+          "updatedRange": "Messages!A16:B16",
+          "updatedRows": 1,
+          "updatedColumns": 2,
+          "updatedCells": 2
+        }
+      }
+    }
+    ```
+
+- **Error Response:**
+
+  - **Code:** `400 Bad Request`
+  - **Content Example:**
+
+    ```json
+    {
+      "message": "Missing one or more required field(s)."
+    }
+    ```
+
+  - **Code:** `500 Internal Server Error`
+  - **Content Example:**
+
+    ```json
+    {
+      "message": "Something went wrong"
+    }
+    ```
+
+#### 2. Get Spreadsheet Data (hardcoded, for testing api authentication)
+
+- Method: `GET`
+- Endpoint: `/sheet/get`
+
+**Response**
+
+- Status: `200`
+- Description: Returns the data from a hardcoded sample spreadsheet ("Messages!A:C").
+
+**Example**
+
+```json
+{
+  "message": {
+    "range": "Messages!A:C",
+    "majorDimension": "ROWS",
+    "values": [
+      ["2023-05-10T09:00:00.000Z", "John Doe", "Hello, this is a test message!"]
+    ]
+  }
+}
+```
--- app.js
+++ app.js
@@ -1,25 +1,92 @@
-require("dotenv").config({ path: "./config/.env" });
-
-//googleAPI and authentication
 const { GoogleAuth } = require("google-auth-library");
 const { google } = require("googleapis");
+require("dotenv").config({ path: "./config/.env" });
 
-//test run the authentication procedure
-fetchDataFromSheets = async ({ sheetName, rangeRC }) => {
-  const auth = new GoogleAuth({
-    scopes: "https://www.googleapis.com/auth/spreadsheets",
-  });
-  const sheets = google.sheets({ version: "v4", auth });
-  const spreadsheetId = process.env.SPREADSHEET_ID;
-  const range = `${sheetName}!${rangeRC}`;
+//express setup
+const express = require("express");
+const app = express();
 
-  const response = await sheets.spreadsheets.values.get({
-    spreadsheetId: spreadsheetId,
-    range: range,
-  });
-  console.log(response.data.values);
+//!------------Middlewares------------
+//*for parsing application/json
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//!-----Functions Used in API Endpoints-----
+const writeData = async ({ spreadsheetLink, valuesToWrite, rangeToWrite }) => {
+  //*extracting spreadsheetId from URL. To consider later: regex might be more robust than the current method.
+  const spreadsheetId = spreadsheetLink.split("/")[5];
+
+  try {
+    const auth = new GoogleAuth({
+      scopes: "https://www.googleapis.com/auth/spreadsheets",
+    });
+    const sheets = google.sheets({ version: "v4", auth });
+
+    const request = {
+      spreadsheetId: spreadsheetId,
+      range: rangeToWrite,
+      valueInputOption: "USER_ENTERED",
+      resource: { values: valuesToWrite },
+    };
+
+    const response = (await sheets.spreadsheets.values.append(request)).data;
+    return response;
+  } catch (err) {
+    throw new Error(err);
+  }
 };
 
-fetchDataFromSheets({ sheetName: "Class Data", rangeRC: "A1:F31" }).catch(
-  console.error
-);
+//!------------Express Listeners------------
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+//hardcoded get function, for testing api authentication
+app.get("/sheet/get", async (req, res) => {
+  try {
+    const auth = new GoogleAuth({
+      scopes: "https://www.googleapis.com/auth/spreadsheets",
+    });
+    const sheets = google.sheets({ version: "v4", auth });
+
+    const request = {
+      spreadsheetId: "1J1wqaKisURMwRKOjNS3e5Bme7KyMahrIB493Vd6_DJg",
+      range: "Messages!A:C",
+    };
+
+    const response = (await sheets.spreadsheets.values.get(request)).data;
+    res.status(200).json({ message: response });
+  } catch (err) {
+    throw new Error(err);
+  }
+});
+
+app.post("/sheet/post", async (req, res) => {
+  try {
+    //*deconstruct and validate request body
+    const { spreadsheetLink, sender, message, rangeToWrite } = req.body;
+    if (!spreadsheetLink || !sender || !message || !rangeToWrite) {
+      return res
+        .status(400)
+        .json({ message: "Missing one or more required field(s)." });
+    }
+
+    const timeStamp = new Date().toISOString();
+    let valuesToWrite = [[timeStamp, sender, message]]; //*resources has to be an array of arrays
+
+    const result = await writeData({
+      spreadsheetLink: spreadsheetLink,
+      rangeToWrite: rangeToWrite,
+      valuesToWrite: valuesToWrite,
+    });
+
+    res.status(200).json({ message: result });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: err });
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}...`);
+});
